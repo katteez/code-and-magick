fc72f1c9f0b1ee116f224f762f95a862fd45d1b6
katteez <ekaterina.zoschik@gmail.com>
2017-02-10 14:37:44 +0300

    Инкапсулировала код ранее созданных модулей

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,23 +1,25 @@
 'use strict';
 
-var ENTER_KEY_CODE = 13;
+window.colorizeElement = (function () {
+  var ENTER_KEY_CODE = 13;
 
-var isEnterKeyPressed = function (event) {
-  return event.keyCode && event.keyCode === ENTER_KEY_CODE;
-};
-
-window.colorizeElement = function (element, colors, property) {
-  var currentColor = element.style[property];
-
-  var changeColor = function () {
-    currentColor = window.utils.getRandomElementExcept(colors, currentColor);
-    element.style[property] = currentColor;
+  var isEnterKeyPressed = function (event) {
+    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
   };
 
-  element.addEventListener('click', changeColor);
-  element.addEventListener('keydown', function () {
-    if (isEnterKeyPressed(event)) {
-      changeColor();
-    }
-  });
-};
+  return function (element, colors, property) {
+    var currentColor = element.style[property];
+
+    var changeColor = function () {
+      currentColor = window.getRandomElementExcept(colors, currentColor);
+      element.style[property] = currentColor;
+    };
+
+    element.addEventListener('click', changeColor);
+    element.addEventListener('keydown', function () {
+      if (isEnterKeyPressed(event)) {
+        changeColor();
+      }
+    });
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,90 +1,94 @@
 'use strict';
 
-var setupOpen = document.querySelector('.setup-open');
-var setupWindow = document.querySelector('.setup');
-var setupClose = setupWindow.querySelector('.setup-close');
-var setupSubmit = setupWindow.querySelector('.setup-submit');
-var wizard = setupWindow.querySelector('#wizard');
-var wizardCoat = wizard.querySelector('#wizard-coat');
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-var wizardEyes = wizard.querySelector('#wizard-eyes');
-var wizardEyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
-var fireball = setupWindow.querySelector('.setup-fireball-wrap');
-var fireballColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
-var ENTER_KEY_CODE = 13;
-var ESCAPE_KEY_CODE = 27;
+(function () {
+  var setupOpen = document.querySelector('.setup-open');
+  var setupWindow = document.querySelector('.setup');
+  var setupClose = setupWindow.querySelector('.setup-close');
+  var setupSubmit = setupWindow.querySelector('.setup-submit');
 
-var isEnterKeyPressed = function (event) {
-  return event.keyCode && event.keyCode === ENTER_KEY_CODE;
-};
+  var wizard = setupWindow.querySelector('#wizard');
+  var wizardCoat = wizard.querySelector('#wizard-coat');
+  var wizardCoatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var wizardEyes = wizard.querySelector('#wizard-eyes');
+  var wizardEyesColors = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
+  var fireball = setupWindow.querySelector('.setup-fireball-wrap');
+  var fireballColors = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
 
-var isEscKeyPressed = function (event) {
-  return event.keyCode && event.keyCode === ESCAPE_KEY_CODE;
-};
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
 
-var closeSetupWindowByEnterKey = function (event) {
-  if (isEnterKeyPressed(event)) {
-    closeSetupWindow();
-  }
-};
+  var isEnterKeyPressed = function (event) {
+    return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+  };
 
-var closeSetupWindowByEscKey = function (event) {
-  if (isEscKeyPressed(event)) {
-    closeSetupWindow();
-  }
-};
+  var isEscKeyPressed = function (event) {
+    return event.keyCode && event.keyCode === ESCAPE_KEY_CODE;
+  };
 
-var showSetupWindow = function () {
-  setupOpen.setAttribute('aria-pressed', true);
-  setupClose.setAttribute('aria-pressed', false);
-  setupWindow.classList.remove('invisible');
-  document.addEventListener('keydown', closeSetupWindowByEscKey);
-  setupClose.addEventListener('click', closeSetupWindow);
-  setupClose.addEventListener('keydown', closeSetupWindowByEnterKey);
-  setupSubmit.addEventListener('click', closeSetupWindow);
-  setupSubmit.addEventListener('keydown', closeSetupWindowByEnterKey);
-};
+  var closeSetupWindowByEnterKey = function (event) {
+    if (isEnterKeyPressed(event)) {
+      closeSetupWindow();
+    }
+  };
 
-var closeSetupWindow = function () {
-  setupClose.setAttribute('aria-pressed', true);
-  setupOpen.setAttribute('aria-pressed', false);
-  setupWindow.classList.add('invisible');
-  document.removeEventListener('keydown', closeSetupWindowByEscKey);
-  setupClose.removeEventListener('click', closeSetupWindow);
-  setupClose.removeEventListener('keydown', closeSetupWindowByEnterKey);
-  setupSubmit.removeEventListener('click', closeSetupWindow);
-  setupSubmit.removeEventListener('keydown', closeSetupWindowByEnterKey);
-};
+  var closeSetupWindowByEscKey = function (event) {
+    if (isEscKeyPressed(event)) {
+      closeSetupWindow();
+    }
+  };
 
-setupOpen.addEventListener('click', function () {
-  showSetupWindow();
-});
+  var closeSetupWindow = function () {
+    setupClose.setAttribute('aria-pressed', true);
+    setupOpen.setAttribute('aria-pressed', false);
+    setupWindow.classList.add('invisible');
+    document.removeEventListener('keydown', closeSetupWindowByEscKey);
+    setupClose.removeEventListener('click', closeSetupWindow);
+    setupClose.removeEventListener('keydown', closeSetupWindowByEnterKey);
+    setupSubmit.removeEventListener('click', closeSetupWindow);
+    setupSubmit.removeEventListener('keydown', closeSetupWindowByEnterKey);
+  };
 
-setupOpen.addEventListener('keydown', function (event) {
-  if (isEnterKeyPressed(event)) {
+  var showSetupWindow = function () {
+    setupOpen.setAttribute('aria-pressed', true);
+    setupClose.setAttribute('aria-pressed', false);
+    setupWindow.classList.remove('invisible');
+    document.addEventListener('keydown', closeSetupWindowByEscKey);
+    setupClose.addEventListener('click', closeSetupWindow);
+    setupClose.addEventListener('keydown', closeSetupWindowByEnterKey);
+    setupSubmit.addEventListener('click', closeSetupWindow);
+    setupSubmit.addEventListener('keydown', closeSetupWindowByEnterKey);
+  };
+
+  setupOpen.addEventListener('click', function () {
     showSetupWindow();
-  }
-});
+  });
 
-window.colorizeElement(wizardCoat, wizardCoatColors, 'fill');
-window.colorizeElement(wizardEyes, wizardEyesColors, 'fill');
-window.colorizeElement(fireball, fireballColors, 'backgroundColor');
+  setupOpen.addEventListener('keydown', function (event) {
+    if (isEnterKeyPressed(event)) {
+      showSetupWindow();
+    }
+  });
+
+  window.colorizeElement(wizardCoat, wizardCoatColors, 'fill');
+  window.colorizeElement(wizardEyes, wizardEyesColors, 'fill');
+  window.colorizeElement(fireball, fireballColors, 'backgroundColor');
+})();
